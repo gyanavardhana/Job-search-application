fda8f4b311a77eaa386d5ddea2483cc6dc2a2494
gyanavardhana <gyanavardhanmamidisetti@gmail.com>
2024-01-16 20:09:12 +0530

    routes

--- jobs/App.js
+++ jobs/App.js
@@ -5,19 +5,15 @@ const session = require('express-session');
 const app = express();
 const port = 3000;
 
-// database connection
-app.set('view engine', 'ejs');
-
-try {
-    mongoose.connect('mongodb://127.0.0.1:27017/jobportal', {})
-        .then(() => console.log("Database Connected"));
-}
-catch (err) {
-    console.log(err);
-}
-
 
 const recroutes = require('./routes/recruiterauth');
+// database connection
+const db = require('./db/databaseconnection');
+app.set('view engine', 'ejs');
+
+
+
+
 
 const Job = require('./Models/jobsmodel');
 const Candidate = require('./Models/candidatemodel');
