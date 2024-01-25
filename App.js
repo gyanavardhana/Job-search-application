3da650f0ba6cf30be78806e8145dd8ed34db4dd9
gyanavardhana <gyanavardhanmamidisetti@gmail.com>
2024-01-25 20:37:18 +0530

    pdf viewer added

--- App.js
+++ App.js
@@ -11,10 +11,15 @@ app.set('view engine', 'ejs');
 
 app.use(recruiterroutes);
 app.use(candidateroutes);
+app.use(express.static('public'));
 
+app.get('/pdf/:file', (req, res) => {
+	res.sendFile(`${__dirname}/public/${req.params.file}`);
+});
 
-    
-
+app.get('/pdf/:file', (req, res) => {
+	res.render('pdf-viewer', { file: req.params.file });
+});
 // all routes
 
 
